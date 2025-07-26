c8560698b54aad0a13fa2f88c79905979d927191
raytrek2-\ktc <>
2025-07-26 17:47:44 +0900

    a

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float angle;
+    [SerializeField] private float sp;
+    [SerializeField] private GameObject baret;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
