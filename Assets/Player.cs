2af0f69b0ac4266cd2ce078a395733e74a450b5a
5F\ktc <>
2025-08-06 15:50:57 +0900

    メモ追加

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -16,6 +16,41 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W))
+        {
+            //前進
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            //後退
+        }
+
+        if(Input.GetKey(KeyCode.Z))
+        {
+            //ロックオン 8体
+        }
+
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            //弾発射
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            //上旋回
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //下旋回
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //左旋回
+        }
+        else if (!Input.GetKey(KeyCode.RightArrow))
+        {
+            //右旋回
+        }
     }
 }
